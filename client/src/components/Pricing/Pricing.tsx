9bf32c7a746f56740fbd0a2f590751992ba59216
ZionJiam <52207958+ZionJiam@users.noreply.github.com>
2025-07-13 02:23:59 +0800

    Update Pricing

--- client/src/components/Pricing/Pricing.tsx
+++ client/src/components/Pricing/Pricing.tsx
@@ -1,117 +1,81 @@
 import { useState } from 'react';
 
-import { PricingContainer, PricingTitle, PackageCardWrapper, 
-    PackageCardGrid, PackageCard, PackageTitle, PackageTitleWrapper, PackageSubtitle, PackageListItemWrapper, PackageListItem, PackagePricing,
-    PopularPackageCard, PopularBadge, BillingToggleWrapper, BillingToggleButton, PackageHostingNote
+import {
+  PricingContainer, PricingTitle, PackageCardWrapper,
+  PackageCardGrid, PackageCard, PackageTitle, PackageTitleWrapper, PackageSubtitle, PackageListItemWrapper, PackageListItem, PackagePricing,
+  PopularPackageCard, PopularBadge, BillingToggleWrapper, BillingToggleButton, PackageHostingNote
 } from './Pricing.style';
 
 
-import { IoCheckmarkSharp } from "react-icons/io5";
+import { IoCheckmarkSharp, IoCloseSharp } from "react-icons/io5";
 import { FaAnglesUp } from "react-icons/fa6";
 
 
 const Pricing = () => {
-    const [billingCycle, setBillingCycle] = useState<'monthly' | 'oneTime'>('monthly');
-  
-    // Pricing values based on billingCycle
-    const prices = {
-      monthly: { essentials: '$48', growth: '$128' },
-      oneTime: { essentials: '$648', growth: '$1688', hosting: '+$15/mo hosting' },
-    };
-  
-    // Example: different features for monthly vs one-time if needed
-    // const features = billingCycle === 'monthly' ? monthlyFeatures : oneTimeFeatures;
-  
-    return (
-      <PricingContainer>
-        <PricingTitle>Plans and Packages</PricingTitle>
-  
-        {/* Tabs */}
-        <BillingToggleWrapper>
-        <BillingToggleButton
-            selected={billingCycle === 'monthly'}
-            onClick={() => setBillingCycle('monthly')}
-        >
-            Monthly
-        </BillingToggleButton>
-        <BillingToggleButton
-            selected={billingCycle === 'oneTime'}
-            onClick={() => setBillingCycle('oneTime')}
-        >
-            One-Time
-        </BillingToggleButton>
-        </BillingToggleWrapper>
-  
-        <PackageCardWrapper>
-          <PackageCardGrid>
-            <PackageCard data-aos="fade-in">
-              <PackageTitleWrapper>
-                <PackageTitle>Starter</PackageTitle>
-                <PackageSubtitle>Perfect foundation for your digital journey</PackageSubtitle>
-              </PackageTitleWrapper>
-              <PackageListItemWrapper>
-                <PackageListItem><IoCheckmarkSharp />Design and Development</PackageListItem>
-                <PackageListItem><IoCheckmarkSharp />Lifetime Support</PackageListItem>
-                <PackageListItem><IoCheckmarkSharp />
-                {billingCycle === 'monthly'
-                    ? 'Unlimited Hosting'
-                    : 'Free 1 Year Hosting'}
-                </PackageListItem>
-                <PackageListItem><IoCheckmarkSharp />1 Fully Responsive page</PackageListItem>
-                <PackageListItem><IoCheckmarkSharp />1 Edit Per Month</PackageListItem>
-                <PackageListItem><IoCheckmarkSharp />{billingCycle === 'monthly'
-                    ? '+$25/mo for each new page'
-                    : '+$300 for each new page'}
-                    </PackageListItem>
+  const [billingCycle, setBillingCycle] = useState<'monthly' | 'oneTime'>('monthly');
 
-                <PackageListItem><IoCheckmarkSharp />{billingCycle === 'monthly'
-                    ? '+$10/mo CMS Section Add on'
-                    : '+$120 CMS Section Add On'}
-                </PackageListItem>
-              </PackageListItemWrapper>
-              <PackagePricing>{prices[billingCycle].essentials} 
-                <PackageHostingNote> 
-                    {billingCycle === 'monthly' ? '/ month' : ''} 
-                    </PackageHostingNote> 
-                </PackagePricing>
-            </PackageCard>
-
-            <PopularPackageCard data-aos="fade-in">
-              <PopularBadge>More Popular</PopularBadge>
-              <PackageTitleWrapper>
-                <PackageTitle>Growth</PackageTitle>
-                <PackageSubtitle>Built to support your expanding business</PackageSubtitle>
-              </PackageTitleWrapper>
-              <PackageListItemWrapper>
-                <PackageListItem><IoCheckmarkSharp />Design and Development</PackageListItem>
-                <PackageListItem><IoCheckmarkSharp />Lifetime Support</PackageListItem>
-                <PackageListItem><IoCheckmarkSharp />
-                {billingCycle === 'monthly'
-                    ? 'Unlimited Hosting'
-                    : 'Free 1 Year Hosting'}
-                </PackageListItem>
-                <PackageListItem><FaAnglesUp />5 Fully Responsive pages</PackageListItem>
-
-                <PackageListItem><FaAnglesUp />5 Edit Per Month</PackageListItem>
-                <PackageListItem><FaAnglesUp />{billingCycle === 'monthly'
-                    ? '+$15/mo for each new page'
-                    : '+$180 for each new page'}
-                </PackageListItem>
-                <PackageListItem><FaAnglesUp />CMS Section Included</PackageListItem>
-              </PackageListItemWrapper>
-              <PackagePricing>{prices[billingCycle].growth}
-              <PackageHostingNote>
-                    {billingCycle === 'monthly'
-                    ? '/ month'
-                    : ''}
-                </PackageHostingNote>
-              </PackagePricing>
-            </PopularPackageCard>
-            
-          </PackageCardGrid>
-        </PackageCardWrapper>
-      </PricingContainer>
-    );
+  // Pricing values based on billingCycle
+  const prices = {
+    monthly: { essentials: '$48', growth: '$168' },
+    oneTime: { essentials: '$868', growth: '$2688', hosting: '+$25/mo hosting' },
   };
-  
-  export default Pricing;
+
+  // Example: different features for monthly vs one-time if needed
+  // const features = billingCycle === 'monthly' ? monthlyFeatures : oneTimeFeatures;
+
+  return (
+    <PricingContainer>
+      <PricingTitle>Plans and Packages</PricingTitle>
+
+
+      <PackageCardWrapper>
+        <PackageCardGrid>
+          {/* One-Time Plan */}
+          <PackageCard data-aos="fade-in">
+            <PackageTitleWrapper>
+              <PackageTitle>One-Time</PackageTitle>
+              <PackageSubtitle>Pay once, launch confidently</PackageSubtitle>
+            </PackageTitleWrapper>
+            <PackageListItemWrapper>
+              <PackageListItem><IoCheckmarkSharp />Design and Development</PackageListItem>
+              <PackageListItem><IoCheckmarkSharp />5 Fully Responsive Pages</PackageListItem>
+              <PackageListItem><IoCheckmarkSharp />+$150 for each additional page</PackageListItem>
+              <PackageListItem><IoCheckmarkSharp />+$300 for Article/Blog</PackageListItem>
+              <PackageListItem><IoCheckmarkSharp />Free 1 Year - 5 Edits per month</PackageListItem>
+              <PackageListItem><IoCheckmarkSharp />Free 1 Year Hosting</PackageListItem>
+              <PackageListItem className="text-muted"><IoCloseSharp /> 24/7 Support</PackageListItem>
+              <PackageListItem className="text-muted"><IoCloseSharp /> Lifetime Updates</PackageListItem>
+            </PackageListItemWrapper>
+            <PackagePricing>$2688
+            </PackagePricing>
+          </PackageCard>
+
+          {/* Monthly Plan */}
+          <PopularPackageCard data-aos="fade-in">
+            <PopularBadge>More Popular</PopularBadge>
+            <PackageTitleWrapper>
+              <PackageTitle>Monthly</PackageTitle>
+              <PackageSubtitle>Spread the cost, stay supported</PackageSubtitle>
+            </PackageTitleWrapper>
+            <PackageListItemWrapper>
+              <PackageListItem><IoCheckmarkSharp />Design and Development</PackageListItem>
+              <PackageListItem><IoCheckmarkSharp />5 Fully Responsive Pages</PackageListItem>
+              <PackageListItem><IoCheckmarkSharp />+$150 for each additional page</PackageListItem>
+              <PackageListItem><IoCheckmarkSharp />+$300 for Article/Blog</PackageListItem>
+              <PackageListItem><FaAnglesUp />Unlimited Edits</PackageListItem>
+              <PackageListItem><FaAnglesUp />Unlimited Hosting</PackageListItem>
+              <PackageListItem><FaAnglesUp /> 24/7 Support</PackageListItem>
+              <PackageListItem><FaAnglesUp /> Lifetime Updates</PackageListItem>
+            </PackageListItemWrapper>
+            <PackagePricing>$168
+              <PackageHostingNote>/ month</PackageHostingNote>
+            </PackagePricing>
+          </PopularPackageCard>
+        </PackageCardGrid>
+      </PackageCardWrapper>
+
+    </PricingContainer>
+  );
+};
+
+export default Pricing;
--- client/src/layouts/AppLayout.style.ts
+++ client/src/layouts/AppLayout.style.ts
@@ -263,3 +263,5 @@ export const GradientText = styled.span`
   background-clip: text;
   color: transparent;
 `;
+
+
