b76be3a51fa7c95a3da59566e26357b7996a6380
Zion Jiam <zionjiam88.18@gmail.com>
2026-04-11 22:36:36 +0800

    Update againm

--- client/src/pages/Portfolio.tsx
+++ client/src/pages/Portfolio.tsx
@@ -1,49 +1,15 @@
 // src/pages/Portfolio.jsx
 import AppLayout from '../layouts/AppLayout';
 import SubBanner from '../components/SubBanner/SubBanner';
-import PortfolioDisplay from '../components/Portfolio/PortfolioDisplay';
 
-const projects = [
-    {
-        id: '1',
-        imageUrl: '/portfolio/portfolio-1.webp',
-        title: 'Educational Services',
-        clientName: 'Maythematics',
-        url: 'https://www.maythematics.com'
-    },
-    // {
-    //     id: '2',
-    //     imageUrl: '/portfolio/portfolio-1.webp',
-    //     title: 'Blog & CMS',
-    //     clientName: 'Beta Media',
-    // },
-    // {
-    //     id: '2',
-    //     imageUrl: '/portfolio/portfolio-1.webp',
-    //     title: 'Blog & CMS',
-    //     clientName: 'Beta Media',
-    // },
-    // {
-    //     id: '2',
-    //     imageUrl: '/portfolio/portfolio-1.webp',
-    //     title: 'Blog & CMS',
-    //     clientName: 'Beta Media',
-    // },
-    // {
-    //     id: '2',
-    //     imageUrl: '/portfolio/portfolio-1.webp',
-    //     title: 'Blog & CMS',
-    //     clientName: 'Beta Media',
-    // },
-    // ... more
-];
+
 
 function Portfolio() {
     return (
         <>
             <AppLayout>
                 <SubBanner />
-                <PortfolioDisplay projects={projects} />
+                {/* <PortfolioDisplay projects={projects} /> */}
 
             </AppLayout>
         </>
