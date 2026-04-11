c8156cf928583b63fc4f6886de3b1665bf070bf8
Zion Jiam <zionjiam88.18@gmail.com>
2026-04-11 22:32:06 +0800

    Update again

--- client/src/components/Portfolio/PortfolioDisplay.tsx
+++ client/src/components/Portfolio/PortfolioDisplay.tsx
@@ -1,5 +1,5 @@
 /** @jsxImportSource @emotion/react */
-import { css } from "@emotion/react";
+// import { css } from "@emotion/react";
 import { styles } from "./PortfolioDisplay.style";
 
 interface Project {
